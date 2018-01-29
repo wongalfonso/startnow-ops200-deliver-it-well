ffb336ecaf417ea05f2ec8af8474e118d285c38c
wongalofonso <alfonsowongsd@gmail.com>
2018-01-29 15:06:55 -0800

    Ready to Deploy

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,14 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('Just Testing Things Out', (done) => {
+    chai.request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(err).not.exist;
+        expect(JSON.stringify(res.text)).to.contain('Hello World');
+        done();
+      });
+    });
 })
